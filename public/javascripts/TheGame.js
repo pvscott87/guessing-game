455555c5cd12648576eba4468fdc857228437725
pvscott87 <pvscott@my.waketech.edu>
2022-07-18 16:42:48 -0400

    fixed the rest function

--- public/javascripts/TheGame.js
+++ public/javascripts/TheGame.js
@@ -10,11 +10,11 @@ let numberToGuess = Math.floor(Math.random() * 100) + 1;
 let numOfGuesses = 1;
 let guesses = [];
 let resetButton;
+alert(numberToGuess);
 
 function play(){
     const guess = parseFloat(document.getElementById("guessField").value);
     guesses.push(guess);
-    alert(numberToGuess);
 
     if (guess === numberToGuess){
         if (numOfGuesses === 1){
@@ -39,8 +39,8 @@ function play(){
         lastResult.textContent = "Try again!";
     }
     numOfGuesses++;
-    guessesField.reset();
-    guessesField.focus();
+    document.getElementById("guessField").value = '';
+    document.getElementById("guessField").focus();
 }
 
 guessesField.addEventListener('click', checkGuess);
@@ -48,24 +48,19 @@ guessesField.addEventListener('click', checkGuess);
 function starNewGame() {
     guessesField.disabled = true;
     submitGuess.disabled = true;
-    resetButton = document.childElement('button');
-    resetButton.textContent = "Play Again?";
-    document.body.appendChild(resetButton);
-    resetButton.addEventListener('click', restartGame);
 }
 
 function restartGame() {
     numOfGuesses = 1;
     const resetParas = document.querySelectorAll('.resultParas p');
     for (const resetPara of resetParas) {
-        resetPara,textContent = '';
+        resetPara.textContent = '';
     }
 
-    resetButton.parentNode.removeChild(resetButton);
     guessesField.disabled = false;
     submitGuess.disabled = false;
-    guessesField.reset();
-    guessesField.focus();
+    document.getElementById("guessField").value = '';
+    document.getElementById("guessField").focus();
     numberToGuess = Math.floor(Math.random() * 100) + 1;
 
 }
